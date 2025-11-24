fe586702c421d3bb00c24f5facaa0fa4e1edde44
Ansh <158992171+ansht1@users.noreply.github.com>
2025-11-24 03:57:45 +0000

    48:45

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+Scss(app)
 
-@app.route('/')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task {self.id}"
+
+@app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    # Add a task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except Exception as error:
+            print(f'ERROR: {error}')
+            return f"ERROR: {error}"
+    # See all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
