7dc8ef0d123698dcf0530d2d08d77bcf0b423593
Ansh <158992171+ansht1@users.noreply.github.com>
2025-11-25 02:04:27 +0000

    1:10:27

--- main.py
+++ main.py
@@ -36,6 +36,29 @@ def index():
         tasks = MyTask.query.order_by(MyTask.created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id: int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect('/')
+    except Exception as error:
+        return f"ERROR: {error}"
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id: int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as error:
+            return f"ERROR: {error}"
+    else:
+        return render_template('edit.html', task=task)
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
